9e96b88f15ee940dcb2e10d06945694aff25ba00
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-16 10:32:46 +0100

    added start method to allow player to start the game

--- game.rb
+++ game.rb
@@ -11,16 +11,40 @@ def get_word
   word
 end
 
-# display words accourding to the word length
 
-word = get_word.split("")
-p word
-stick = ""
-word.each{ |char| stick += "_" }
-puts stick
-# get user input and update the stick
-input = gets.chomp
-#if the word include the selcted char display it on the stick
-word.each_with_index do |char, index|
-  stick[index] = input if char == input
+def update_stick( input, word, stick, incorrect_chars)
+  if word.include?(input)
+    word.each_with_index do |char, index|
+      stick[index] = input if char == input
+    end
+  else
+    incorrect_chars << input unless incorrect_chars.include?(input)
+  end
 end
+
+def start
+  word = get_word.split("")
+  p word
+  # display words accourding to the word length
+  stick = ""
+  word.each{ |char| stick += "_" }
+  puts stick
+  incorrect_chars = []
+  #every turn allow the player to make a guess of letter
+  11.times do
+    # get user input and update the stick
+    #if the word include the selcted char display it on the stick, if it is not display incorrect words
+    print "Enter character: "
+    input = gets.chomp
+    update_stick(input, word, stick, incorrect_chars)
+    puts stick
+    puts "incorrect letters so far: #{incorrect_chars.join(" ")} " if incorrect_chars.size > 0
+    if stick == word.join("")
+      puts "you win"
+      return
+    end
+  end
+  puts "you lose"
+end
+
+start
